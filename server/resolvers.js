01d11c2d452137624afb7c3d2d52d9812e483c00
Mirko Nasato <mirko@nasato.dev>
2022-04-25 18:15:56 +0100

    updateJob Mutation

--- server/resolvers.js
+++ server/resolvers.js
@@ -10,6 +10,7 @@ export const resolvers = {
   Mutation: {
     createJob: (_root, { input }) => Job.create(input),
     deleteJob: (_root, { id }) => Job.delete(id),
+    updateJob: (_root, { input }) => Job.update(input),
   },
 
   Company: {
