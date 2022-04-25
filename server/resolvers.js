cd843adc75f7c160ff858cb183efea013f3bcd1a
Mirko Nasato <mirko@nasato.dev>
2022-04-25 11:30:30 +0100

    Job-Company Association

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,7 +1,11 @@
-import { Job } from './db.js';
+import { Company, Job } from './db.js';
 
 export const resolvers = {
   Query: {
     jobs: () => Job.findAll(),
+  },
+
+  Job: {
+    company: (job) => Company.findById(job.companyId),
   },
 };
