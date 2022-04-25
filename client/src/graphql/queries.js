f3f4c0666569bf7f887d1b5e9010b075ea4dc1c6
Mirko Nasato <mirko@nasato.dev>
2022-04-25 18:06:06 +0100

    Mutation Request

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -2,6 +2,19 @@ import { request, gql } from 'graphql-request';
 
 const GRAPHQL_URL = 'http://localhost:9000/graphql';
 
+export async function createJob(input) {
+  const query = gql`
+    mutation CreateJobMutation($input: CreateJobInput!) {
+      job: createJob(input: $input) {
+        id
+      }
+    }
+  `;
+  const variables = { input };
+  const { job } = await request(GRAPHQL_URL, query, variables);
+  return job;
+}
+
 export async function getCompany(id) {
   const query = gql`
     query CompanyQuery($id: ID!) {
