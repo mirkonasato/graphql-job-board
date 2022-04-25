850567b2373e9d866af220dbcf11d181b1127dcc
Mirko Nasato <mirko@nasato.dev>
2022-04-25 13:33:18 +0100

    Query Variables

--- client/src/components/JobDetail.js
+++ client/src/components/JobDetail.js
@@ -1,11 +1,19 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
-import { jobs } from '../fake-data';
+import { getJob } from '../graphql/queries';
 
 function JobDetail() {
+  const [job, setJob] = useState(null);
   const { jobId } = useParams();
+  useEffect(() => {
+    getJob(jobId).then(setJob);
+  }, [jobId]);
 
-  const job = jobs.find((job) => job.id === jobId);
+  console.log('[JobDetail] job:', job);
+  if (!job) {
+    return <p>Loading...</p>;
+  }
   return (
     <div>
       <h1 className="title">
--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -2,6 +2,25 @@ import { request, gql } from 'graphql-request';
 
 const GRAPHQL_URL = 'http://localhost:9000/graphql';
 
+export async function getJob(id) {
+  const query = gql`
+    query JobQuery($id: ID!) {
+      job(id: $id) {
+        id
+        title
+        company {
+          id
+          name
+        }
+        description
+      }
+    }
+  `;
+  const variables = { id };
+  const { job } = await request(GRAPHQL_URL, query, variables);
+  return job;
+}
+
 export async function getJobs() {
   const query = gql`
     query {
