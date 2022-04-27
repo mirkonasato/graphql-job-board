86e3c43527f80fecbe609eda05782e323895599a
Mirko Nasato <mirko@nasato.dev>
2022-04-27 11:15:59 +0100

    Cache Manipulation

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -8,11 +8,31 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+const JOB_QUERY = gql`
+  query JobQuery($id: ID!) {
+    job(id: $id) {
+      id
+      title
+      company {
+        id
+        name
+      }
+      description
+    }
+  }
+`;
+
 export async function createJob(input) {
   const mutation = gql`
     mutation CreateJobMutation($input: CreateJobInput!) {
       job: createJob(input: $input) {
         id
+        title
+        company {
+          id
+          name
+        }
+        description
       }
     }
   `;
@@ -20,7 +40,18 @@ export async function createJob(input) {
   const context = {
     headers: { 'Authorization': 'Bearer ' + getAccessToken() },
   };
-  const { data: { job } } = await client.mutate({ mutation, variables, context });
+  const { data: { job } } = await client.mutate({
+    mutation,
+    variables,
+    context,
+    update: (cache, { data: { job } }) => {
+      cache.writeQuery({
+        query: JOB_QUERY,
+        variables: { id: job.id },
+        data: { job },
+      });
+    },
+  });
   return job;
 }
 
@@ -44,21 +75,11 @@ export async function getCompany(id) {
 }
 
 export async function getJob(id) {
-  const query = gql`
-    query JobQuery($id: ID!) {
-      job(id: $id) {
-        id
-        title
-        company {
-          id
-          name
-        }
-        description
-      }
-    }
-  `;
   const variables = { id };
-  const { data: { job } } = await client.query({ query, variables });
+  const { data: { job } } = await client.query({
+    query: JOB_QUERY,
+    variables,
+  });
   return job;
 }
 
