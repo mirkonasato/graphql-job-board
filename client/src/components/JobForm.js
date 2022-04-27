0ef5456d2ca22b67e82171c8c7fae1cf69ec51ed
Mirko Nasato <mirko@nasato.dev>
2022-04-27 13:50:02 +0100

    useMutation Hook

--- client/src/components/JobForm.js
+++ client/src/components/JobForm.js
@@ -1,16 +1,30 @@
+import { useMutation } from '@apollo/client';
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
-import { createJob } from '../graphql/queries';
+import { getAccessToken } from '../auth';
+import { CREATE_JOB_MUTATION, JOB_QUERY } from '../graphql/queries';
 
 function JobForm() {
   const navigate = useNavigate();
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
+  const [mutate] = useMutation(CREATE_JOB_MUTATION);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const job = await createJob({ title, description });
-    console.log('job created:', job);
+    const { data: { job } } = await mutate({
+      variables: { input: { title, description } },
+      context: {
+        headers: { 'Authorization': 'Bearer ' + getAccessToken() },
+      },
+      update: (cache, { data: { job } }) => {
+        cache.writeQuery({
+          query: JOB_QUERY,
+          variables: { id: job.id },
+          data: { job },
+        });
+      },
+    });
     navigate(`/jobs/${job.id}`);
   };
 
--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -1,5 +1,4 @@
 import { ApolloClient, gql, InMemoryCache } from '@apollo/client';
-import { getAccessToken } from '../auth';
 
 const GRAPHQL_URL = 'http://localhost:9000/graphql';
 
@@ -56,30 +55,11 @@ export const JOBS_QUERY = gql`
   }
 `;
 
-export async function createJob(input) {
-  const mutation = gql`
-    mutation CreateJobMutation($input: CreateJobInput!) {
-      job: createJob(input: $input) {
-        ...JobDetail
-      }
+export const CREATE_JOB_MUTATION = gql`
+  mutation CreateJobMutation($input: CreateJobInput!) {
+    job: createJob(input: $input) {
+      ...JobDetail
     }
-    ${JOB_DETAIL_FRAGMENT}
-  `;
-  const variables = { input };
-  const context = {
-    headers: { 'Authorization': 'Bearer ' + getAccessToken() },
-  };
-  const { data: { job } } = await client.mutate({
-    mutation,
-    variables,
-    context,
-    update: (cache, { data: { job } }) => {
-      cache.writeQuery({
-        query: JOB_QUERY,
-        variables: { id: job.id },
-        data: { job },
-      });
-    },
-  });
-  return job;
-}
+  }
+  ${JOB_DETAIL_FRAGMENT}
+`;
