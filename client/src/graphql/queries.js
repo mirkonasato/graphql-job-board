f65ce4980ec0f06f1787962b99644ec2b9a026ef
Mirko Nasato <mirko@nasato.dev>
2022-04-27 10:38:05 +0100

    Query Method

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -1,8 +1,13 @@
-import { gql } from '@apollo/client';
+import { ApolloClient, gql, InMemoryCache } from '@apollo/client';
 import { request } from 'graphql-request';
 import { getAccessToken } from '../auth';
 
 const GRAPHQL_URL = 'http://localhost:9000/graphql';
+
+const client = new ApolloClient({
+  uri: GRAPHQL_URL,
+  cache: new InMemoryCache(),
+});
 
 export async function createJob(input) {
   const query = gql`
@@ -58,7 +63,7 @@ export async function getJob(id) {
 
 export async function getJobs() {
   const query = gql`
-    query {
+    query JobsQuery {
       jobs {
         id
         title
@@ -68,6 +73,6 @@ export async function getJobs() {
       }
     }
   `;
-  const { jobs } = await request(GRAPHQL_URL, query);
+  const { data: { jobs } } = await client.query({ query });
   return jobs;
 }
